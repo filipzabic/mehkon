b64b03bb19609b3bfd2249227e9f7e123cf8cb92
Filip Žabić <71082911+filipzabic@users.noreply.github.com>
2022-06-16 15:51:18 +0200

    Added working prototype code

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, request, jsonify, make_response, send_file
 from flask import render_template as template
 #from waitress import serve
-from sympy import symbols, init_printing, latex, Matrix
+from sympy import symbols, init_printing, latex, Matrix, tan, diff, zeros
+from utils import calculate_cube_side, calculate_gradient_tensor, \
+                  calculate_left_green_cauchy_tensor, \
+                  calculate_right_green_cauchy_tensor, \
+                  calculate_euler_tensor, \
+                  calculate_lagrange_tensor
 
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -9,23 +14,63 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    x = symbols('x')
-    x = x**2
-    F = Matrix([x, x])
+
+    F = zeros(3)
+    b = zeros(3)
+    e = zeros(3)
+    C = zeros(3)
+    E = zeros(3)
+
     F = latex(F)
-    
-    return template('index.html', var=F)
+    b = latex(b)
+    e = latex(e)
+    C = latex(C)
+    E = latex(E)
+
+    return template('index.html', F=F, b=b, C=C, e=e, E=E)
 
 
 @app.post('/')
 def index_post():
-    hours_worked = int(request.form['hours_worked'])
-    hourly_pay = int(request.form['hourly_pay'])
-    date_from = request.form['date_from']
-    date_to = request.form['date_to']
-    other_fees = int(request.form['other_fees'])
+    a = float(request.form['a'])
 
-    return template('index.html', var=F)
+    alpha = float(request.form['alpha'])
+    beta = float(request.form['beta'])
+    gamma = float(request.form['gamma'])
+
+    volume = a**3
+
+    #selection = int(request.form['selection'])
+
+    X1, X2, X3 = symbols('X1 X2 X3')
+    x1, x2, x3 = symbols('x1 x2 x3')
+
+    #vlak x1
+    x = calculate_cube_side(volume, a*alpha, a*beta, 10)
+
+    u1 = tan(gamma)*X2
+    u2 = 0
+    u3 = 0
+
+    x1 = X1 + u1
+    x2 = X2 + u2
+    x3 = X3 + u3
+
+    F = calculate_gradient_tensor(x1, x2, x3, X1, X2, X3)
+    b = calculate_left_green_cauchy_tensor(F)
+    C = calculate_right_green_cauchy_tensor(F)
+    e = calculate_euler_tensor(b)
+    E = calculate_lagrange_tensor(C)
+
+    F = latex(F)
+    b = latex(b)
+    e = latex(e)
+    C = latex(C)
+    E = latex(E)
+
+
+
+    return template('index.html', F=F, b=b, C=C, e=e, E=E)
 
 
 @app.errorhandler(404)
--- utils.py
+++ utils.py
@@ -1,38 +1,43 @@
 import numpy as np
-from sympy import symbols, diff, Matrix
+from sympy import symbols, diff, Matrix, Identity, eye
 
 
-def calculate_cube_side(volume, a1, a2):
-    return volume/(a1*a2)
-
-a = 10
-alpha = 1.2
-beta = 1.3
-gamma = 0.1
-volume = a**3
-
-X1, X2, X3 = symbols('X1 X2 X3')
-x1, x2, x3 = symbols('x1 x2 x3')
-
-#vlak x1
-u1 = alpha*a
-u2 = 0
+def calculate_cube_side(volume, a1, a2, base):
+    return volume/(a1*a2*base)
 
 
-F1_1 = diff(x1, X1)
-F1_2 = diff(x1, X2)
-F1_3 = diff(x1, X3)
+def calculate_gradient_tensor(x1, x2, x3, X1, X2, X3):
 
-F2_1 = diff(x2, X1)
-F2_2 = diff(x2, X2)
-F2_3 = diff(x2, X3)
+    F1_1 = diff(x1, X1)
+    F1_2 = diff(x1, X2)
+    F1_3 = diff(x1, X3)
 
-F3_1 = diff(x3, X1)
-F3_2 = diff(x3, X2)
-F3_3 = diff(x3, X3)
+    F2_1 = diff(x2, X1)
+    F2_2 = diff(x2, X2)
+    F2_3 = diff(x2, X3)
 
-F = Matrix([[F1_1, F1_2, F1_3],
-            [F2_1, F2_2, F2_3],
-            [F3_1, F3_2, F3_3]])
+    F3_1 = diff(x3, X1)
+    F3_2 = diff(x3, X2)
+    F3_3 = diff(x3, X3)
 
-print(F)
+    F = Matrix([[F1_1, F1_2, F1_3],
+                [F2_1, F2_2, F2_3],
+                [F3_1, F3_2, F3_3]])
+    
+    return F
+
+
+def calculate_lagrange_tensor(C):
+    return 0.5*(C-eye(3))
+
+
+def calculate_euler_tensor(b):
+    return 0.5*(eye(3)-b.inv())
+
+
+def calculate_left_green_cauchy_tensor(F):
+    return F*F.T
+
+
+def calculate_right_green_cauchy_tensor(F):
+    return F.T*F
